7dc7ee5b2c7e84da1de3a69dc6fad15de2ed80dc
Vimala Rajkumar <160380604+Vimala2020@users.noreply.github.com>
2024-03-23 13:06:20 +0530

    updated project

---
+++ src/component/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+      <>
+        <h1>Perfumy</h1>
+        <ul>
+          <li>Home |</li>
+          <li>Products |</li>
+          <li>About |</li>
+          <li>Contact Us |</li>
+        </ul>
+      </>
+    )
+  }
+  export default Navbar
